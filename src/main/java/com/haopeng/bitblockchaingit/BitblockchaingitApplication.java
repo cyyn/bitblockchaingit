b53f0da42e5bf371d37f124c1f2cfdf02f2e5a99
cyyn <2908464947@qq.com>
2019-04-16 19:29:41 +0800

    bit first

---
+++ src/main/java/com/haopeng/bitblockchaingit/BitblockchaingitApplication.java
@@ -0,0 +1,13 @@
+package com.haopeng.bitblockchaingit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitblockchaingitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitblockchaingitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haopeng/bitblockchaingit/BitblockchaingitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.haopeng.bitblockchaingit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BitblockchaingitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
