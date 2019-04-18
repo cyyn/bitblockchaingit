f053653d989cb378a9cae0e5a14a1b3de5192c58
cyyn <2908464947@qq.com>
2019-04-18 13:18:55 +0800

    bit updata api and controller

--- src/main/java/com/haopeng/bitblockchaingit/BitblockchaingitApplication.java
+++ src/main/java/com/haopeng/bitblockchaingit/BitblockchaingitApplication.java
@@ -2,8 +2,10 @@ package com.haopeng.bitblockchaingit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BitblockchaingitApplication {
 
     public static void main(String[] args) {
